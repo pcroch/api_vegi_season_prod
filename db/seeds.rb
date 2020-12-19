5d3448549669813480deb4dbcef98f94cc0e4f7a
pierre crochet <pierrecrochet03@gmail.com>
2020-12-19 20:06:32 +0000

    seed v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.create(email: 'admin@admin.admin', password: '123123' )
+User.create(email: 'admin@admin.admin', password: '123123' )
 
-
-product1 = Product.create(name: 'Apple', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user_id: 1 )
-# product2 = Product.create(name: 'Peer', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user: admin )
-# product3 = Product.create(name: 'Grape', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user: admin )
-# product4 = Product.create(name: 'Tomatoes', kind: 'vegetable', months_available: 'April', seasons_available: 'summer', user: admin )
-# product5 = Product.create(name: 'Brocoli', kind: 'vegetable', months_available: 'August', seasons_available: 'summer', user: admin )
-# product6 = Product.create(name: 'Cauliflower', kind: 'vegetable', months_available: 'September', seasons_available: 'autumn', user: admin )
-
+Product.create(name: 'Apple', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user_id: 1)
+Product.create(name: 'Peer', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user_id: 1)
+Product.create(name: 'Grape', kind: 'fruit', months_available: 'September', seasons_available: 'autumn', user_id: 1)
+Product.create(name: 'Tomatoes', kind: 'vegetable', months_available: 'April', seasons_available: 'summer', user_id: 1)
+Product.create(name: 'Brocoli', kind: 'vegetable', months_available: 'August', seasons_available: 'summer', user_id: 1)
+Product.create(name: 'Cauliflower', kind: 'vegetable', months_available: 'September', seasons_available: 'autumn', user_id: 1)
