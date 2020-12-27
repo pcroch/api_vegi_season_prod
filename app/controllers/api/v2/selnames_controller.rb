750089b742cba43c0a1e6e42f658415f388bdd0f
pierre crochet <pierrecrochet03@gmail.com>
2020-12-27 19:57:49 +0100

    removing useless css and usless comments

--- app/controllers/api/v2/selnames_controller.rb
+++ app/controllers/api/v2/selnames_controller.rb
@@ -3,7 +3,7 @@ class Api::V2::SelnamesController < Api::V2::BaseController
   before_action :set_selnames, only: [:show]
 
   def show
-    # render json: { product_month: @month, status: :success }
+
   end
 
   private
@@ -12,15 +12,4 @@ class Api::V2::SelnamesController < Api::V2::BaseController
     @selname = Product.where(name: params[:id])
     authorize @selname  # For Pundit
   end
-
-  # def selname_params
-  #   params.require(:selname).permit(:name, :kind, :months_available)
-  # end
-
-  # def render_error
-  #   render json: { errors: @selname.errors.full_messages },
-  #   status: :unprocessable_entity
-  # end
-
-
 end
--- app/models/month.rb
+++ app/models/month.rb
@@ -5,3 +5,5 @@ end
 
 #  I dont have nay idea how to create new action? Do you have exp about that?
 # stuck with  selecting oject of api withoutceeqting a totally new model routes...? easier way?
+# I feel my apis are useless? nothing hard exept copy pasting my job around the model...
+# I need imagination to do something harder
